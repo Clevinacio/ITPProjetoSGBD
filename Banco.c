b44fed24f0c930a12c6264268f9e6a7387ed2bae
Cleverton Inacio <clev.inacio@hotmail.com>
2018-11-12 20:28:11 -0300

    Modularização das funções de verificação das tabelas e colunas.

--- Banco.c
+++ Banco.c
@@ -7,14 +7,18 @@
   #                         Nataly Enne da Costa Gonçalo         #
   ################################################################
 */
+
+void verificarTabela(char *nomeTabela);
+
+void verificaColuna(int i, char **colunas, char *coluna);
+
 int main(int argc, char const *argv[]) {
   // metadados é um arquivo que vai salvar o nome da tabela, as colunas e as
   // linhas.
   char *nomeArquivo = "Metadados.itp", *nomeTabela = malloc(sizeof(char) * 200),
        *primaryKey = malloc(sizeof(char) * 100), ext[] = ".itp",
        *coluna = malloc(sizeof(char) * 100);
-  int quantColunas,
-      comparador; // comparador usado para gerenciar comparação das colunas
+  int quantColunas;
   // abrindo metadados: escreve no arquivo, lê o arquivo.
   FILE *metadados = fopen(nomeArquivo, "a+"), *arquivoTabela;
   if (metadados != NULL) { // Se arquivo NÂO estiver vazio: IF iniciado.
@@ -22,18 +26,8 @@ int main(int argc, char const *argv[]) {
     printf("Digite o nome da tabela: ");
     scanf("%s", nomeTabela); // recebe do USUÁRIO o nome da tabela.
 
-    char *buffer = malloc(sizeof(char) * 100);
-    // lê o arquivo até a quebra de linha  e entra no IF para checá-lo.
-    while (fscanf(metadados, "%s", buffer) != EOF) {
+    verificarTabela(nomeTabela);
 
-      /* ENQUANTO nome da tabela inserido pelo USUÁRIO for igual a nome de
-      alguma tabela existente no arquivo, ele recusa e pede um nome diferente
-      para a tabela. */
-      while (strcmp(nomeTabela, buffer) == 0) {
-        printf("Tabela já existe! Dê outro nome: ");
-        scanf("%s", nomeTabela); // recebe o novo nome.
-      }
-    }
     /* caso entre no IF e o nome de tabela INSERIDO pelo USUÁRIO seja
        diferente de algu nome de tabela do arquivo ele sai do
        while e cria o arquivo da tabela */
@@ -48,8 +42,7 @@ int main(int argc, char const *argv[]) {
     scanf("%d", &quantColunas); // solicitação de quantidade de colunas
     char *colunas[quantColunas];
     for (int i = 0; i < quantColunas; i++) {
-      comparador =
-          0; // define o comparador como zero para verificar todas as colunas
+      // define o comparador como zero para verificar todas as colunas
       colunas[i] =
           malloc(sizeof(char) * 100); // aloca memória para o vetor que irá
                                       // armazenar o nome das colunas
@@ -59,20 +52,14 @@ int main(int argc, char const *argv[]) {
       } else {
         printf("Digite o nome da coluna %d: ", i + 1);
         scanf("%s", coluna); // recebe o nome da coluna
-        while (comparador < i) {
-          if (strcmp(coluna, colunas[comparador]) == 0) {
-            printf("Coluna existente! Digite um novo nome: ");
-            scanf("%s", coluna); // recebe o nome da coluna
-            comparador = 0;
-          } else {
-            comparador++;
-          }
-        }
-        strcpy(colunas[i], coluna);
+
+        verificaColuna(i, colunas, coluna);
+        strcpy(colunas[i],
+               coluna); // passa valor digitador pra array de strings
       }
     }
 
-    for (size_t i = 0; i < quantColunas; i++) {
+    for (size_t i = 0; i < quantColunas; i++) { // inserir colunas no arquivo
       if (i == 0) {
         fprintf(arquivoTabela, "%s*|", colunas[i]);
       } else
@@ -84,10 +71,38 @@ int main(int argc, char const *argv[]) {
     fclose(metadados);
     free(nomeTabela);
     free(primaryKey);
-    free(buffer);
     free(coluna);
   } else
     printf("Erro na leitura ou criação do arquivo\n");
 
   return 0;
+}
+
+void verificarTabela(char *nomeTabela) {
+  FILE *metadados = fopen("Metadados.itp", "a+"), *arquivoTabela;
+  char *buffer = malloc(sizeof(char) * 100);
+  // lê o arquivo até a quebra de linha  e entra no IF para checá-lo.
+  while (fscanf(metadados, "%s", buffer) != EOF) {
+
+    /* ENQUANTO nome da tabela inserido pelo USUÁRIO for igual a nome de
+    alguma tabela existente no arquivo, ele recusa e pede um nome diferente
+    para a tabela. */
+    while (strcmp(nomeTabela, buffer) == 0) {
+      printf("Tabela já existe! Dê outro nome: ");
+      scanf("%s", nomeTabela); // recebe o novo nome.
+    }
+  }
+  free(buffer);
+}
+void verificaColuna(int i, char **colunas, char *coluna) {
+  int comparador = 0; // comparador usado para gerenciar comparação das colunas
+  while (comparador < i) {
+    if (strcmp(coluna, colunas[comparador]) == 0) {
+      printf("Coluna existente! Digite um novo nome: ");
+      scanf("%s", coluna); // recebe o nome da coluna
+      comparador = 0;
+    } else {
+      comparador++;
+    }
+  }
 }
